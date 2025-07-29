6e4d7a7932ac167356a4f09876c917c2d64579d8
divyanshV <divyansh.verma@idemia.com>
2025-07-29 22:16:23 +0530

    initial

---
+++ bin/tdd_algorithms.dart
@@ -0,0 +1,5 @@
+import 'package:tdd_algorithms/tdd_algorithms.dart' as tdd_algorithms;
+
+void main(List<String> arguments) {
+  print('Hello world: ${tdd_algorithms.calculate()}!');
+}
---
+++ test/tdd_algorithms_test.dart
@@ -0,0 +1,8 @@
+import 'package:tdd_algorithms/tdd_algorithms.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
